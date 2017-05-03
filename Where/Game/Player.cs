78631ad05a35defc67e295e38128af1860dba3ef
星翼 <853974536@qq.com>
2017-05-03 22:44:26 +0800

    Player Move Tween

--- Where/Game/Player.cs
+++ Where/Game/Player.cs
@@ -11,31 +11,41 @@ namespace Where.Game
         {
             renderer = rnd;
             angle = 0;
-            position = pos;
+            speed = 0;
+            Position = pos;
         }
 
         public override void OnUpdate()
         {
-            renderer.SetCamera(angle,position);
+            renderer.SetCamera(angle,Position);
             angle += Input.Roller.XMove / 4;
 
             var s = Input.Runner.State;
+
             if (s != Input.Runner.StateType.Stop)
             {
-                Vector2 delta = new Vector2(
-                    (float)Math.Sin(angle*3.1415926f/180.0),
-                    (float)Math.Cos(angle*3.1415926f/180.0)
-                );
-
-                delta *= -1.0f;
-
-                position += s == Input.Runner.StateType.Go ? delta : -delta;
+                speed = s == Input.Runner.StateType.Go ? 0.5f : -0.5f;
             }
 
+            if(Math.Abs(speed) > 0.0f)
+            {
+                speed *= 0.7f;
+                if (Math.Abs(speed) < 0.05f)
+                    speed = 0.0f;
+            }
+
+            Vector2 delta = new Vector2(
+                (float)Math.Sin(angle * 3.1415926f / 180.0f),
+                (float)Math.Cos(angle * 3.1415926f / 180.0f)
+            );
+
+            delta *= -1.0f * speed;
+
+            Position += delta;
         }
 
-        Vector2 position;
-        float angle;
+        public Vector2 Position { get; set; }
+        float angle,speed;
         readonly Renderer.IRenderer renderer;
     }
 }
