78046eb3896586c4222ddb846d2841140132cbc7
星翼 <853974536@qq.com>
2017-05-16 14:15:38 +0800

    垂直雾+深度雾

--- Where/Shaders/3D_ObjectDraw.fs
+++ Where/Shaders/3D_ObjectDraw.fs
@@ -1,11 +1,23 @@
 #version 110
 
 varying vec2 TexCoord;
+varying vec3 WorldPos;
+varying vec3 CameraPos;
 
 uniform sampler2D Tex1;
 uniform sampler2D Tex2;
 uniform sampler2D Tex3;
 
+
+
 void main(){
-	gl_FragColor = vec4(sin(TexCoord.x*256.0),cos(512.0*TexCoord.y),1.0,1.0);
+	float densityA = clamp(WorldPos.y / 100.0,0.0,1.0);
+	float densityB = 1.0-clamp(CameraPos.z / 80.0,0.0,1.0);
+	float density = densityA * densityB;
+	vec4 fogColor = vec4(1.0);
+	vec4 color = vec4(sin(TexCoord.x*256.0),cos(512.0*TexCoord.y),1.0,1.0);
+	
+	vec4 foged = mix(fogColor,color,density);
+	
+	gl_FragColor = foged;//mix(foged,color,0.05);
 }
--- Where/Shaders/VertexShader.vs
+++ Where/Shaders/VertexShader.vs
@@ -5,8 +5,12 @@ attribute vec2 TexCoordInput;
 uniform mat4 Camera;
 
 varying vec2 TexCoord;
+varying vec3 WorldPos;
+varying vec3 CameraPos;
 
 void main(){
 	TexCoord = TexCoordInput;
+	WorldPos = Vertex.xyz;
 	gl_Position = Camera * Vertex;
+	CameraPos = gl_Position.xyz;
 }
