d7ee03c396f5253aa8d777c302c0353273f35673
星翼 <853974536@qq.com>
2017-06-23 19:22:52 +0800

    后处理系统修正

---
+++ Where/Renderer/Renderer3D/AfterEffect/AfterEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Where.Renderer.Renderer3D.AfterEffect
+{
+    interface IAfterEffect
+    {
+        void SetDrawState();
+        void ResetDrawState();
+        int DownSample {get;}
+        int Live { get; }
+    }
+}
---
+++ Where/Renderer/Renderer3D/AfterEffect/AfterEffectSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.ES20;
+
+namespace Where.Renderer.Renderer3D.AfterEffect
+{
+    class AfterEffectSystem
+    {
+        public AfterEffectSystem()
+        {
+            texHandle = GL.GenTexture();
+        }
+
+        public void OnDraw()
+        {
+            foreach(var i in effects)
+            {
+                GL.CopyTexImage2D(TextureTarget2d.Texture2D, i.DownSample, TextureCopyComponentCount.Rgba, 0, 0, Engine.Engine.Window.Width, Engine.Engine.Window.Height, 0);
+            }
+        }
+
+        public void AddEffect(IAfterEffect effect)
+        {
+            effects.Add(effect);
+        }
+
+        int texHandle;
+        List<IAfterEffect> effects;
+    }
+}
